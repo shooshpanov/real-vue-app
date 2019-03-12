ee105381f4fb4afe6d4041c26f3ff710ebc2a0f8
Sergey S <shooshpanov@yandex.com>
2019-03-12 12:06:11 -0400

    Modules refactoring

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
-    <h1>Event Listing</h1>
-    <event-card v-for="event in events" :key="event.id" :event="event" />
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
     <template v-if="page != 1">
       <router-link
         :to="{ name: 'event-list', query: { page: page - 1 } }"
         rel="prev"
-        >Prev</router-link
       >
+        Prev Page</router-link
+      >
+      <template v-if="hasNextPage">
+        |
+      </template>
     </template>
-    |
     <router-link
+      v-if="hasNextPage"
       :to="{ name: 'event-list', query: { page: page + 1 } }"
       rel="next"
-      >Next</router-link
+    >
+      Next Page</router-link
     >
   </div>
 </template>
+
 <script>
 import EventCard from '@/components/EventCard.vue'
 import { mapState } from 'vuex'
@@ -26,8 +32,11 @@ export default {
     EventCard
   },
   created() {
-    this.$store.dispatch('fetchEvents', {
-      perPage: 3,
+    this.perPage = 3 // Setting perPage here and not in data means it won't be reactive.
+    // We don't need it to be reactive, and this way our component has access to it.
+
+    this.$store.dispatch('event/fetchEvents', {
+      perPage: this.perPage,
       page: this.page
     })
   },
@@ -35,7 +44,10 @@ export default {
     page() {
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events'])
+    hasNextPage() {
+      return this.event.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['event', 'user'])
   }
 }
 </script>
